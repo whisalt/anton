9f2f5e49bd2efe11db79ca8635472c96f752409e
whisalt <antonrazinkov00@gmail.com>
2024-12-14 10:38:44 +0400

    Добавлен main.py с кнопками

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Приложение без кнопок")
 
-label = tk.Label(root, text="Это приложение без кнопок.")
+def button_action():
+    print("Кнопка нажата!")
+
+
+root = tk.Tk()
+root.title("Приложение с кнопками")
+
+label = tk.Label(root, text="Это приложение с кнопками.")
 label.pack()
+
+button = tk.Button(root, text="Нажми меня!", command=button_action)
+button.pack()
 
 root.mainloop()
 
